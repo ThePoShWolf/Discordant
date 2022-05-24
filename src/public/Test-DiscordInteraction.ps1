1a2b7326c64c6513bd80ca26622c998c341f1ac6
Anthony Howell <anthony@howell-it.com>
2022-05-24 15:13:07 -0700

    now tests without auth

--- src/public/Test-DiscordInteraction.ps1
+++ src/public/Test-DiscordInteraction.ps1
@@ -7,5 +7,8 @@ Function Test-DiscordInteraction {
         [string]$TimeStamp,
         [string]$Body
     )
+    if (-not (Get-Variable -Name DiscordClient -Scope Global -ErrorAction Ignore)) {
+        $DiscordClient = [Discord.Rest.DiscordRestClient]::new()
+    }
     $DiscordClient.IsValidHttpInteraction($PublicKey, $Signature, $TimeStamp, $Body)
 }
